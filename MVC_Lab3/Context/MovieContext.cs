1a22983ed40e990d2fa7cda7eb639d034de2a5ec
katunknox <48958237+katunknox@users.noreply.github.com>
2021-03-18 09:53:30 +1100

    Add project files.

---
+++ MVC_Lab3/Context/MovieContext.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Lab3.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MVC_Lab3.Context
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+            : base("MovieContext")
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MVC_Lab3/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MVC_Lab3.Models
+{
+    public class Movie
+    {
+        //Add your properties here.
+    }
+}
