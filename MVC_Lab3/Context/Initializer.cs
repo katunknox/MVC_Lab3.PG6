35d1dfba1b4409b8ba557d51980cc819c3bcaa3a
katunknox <48958237+katunknox@users.noreply.github.com>
2021-03-18 10:03:53 +1100

    ny

--- MVC_Lab3/Context/Initializer.cs
+++ MVC_Lab3/Context/Initializer.cs
@@ -10,65 +10,65 @@ namespace MVC_Lab3.Context
     {
         protected override void Seed(MovieContext context)
         {
-            //var seededMovies = new List<Movie>
-            //{
-            //     new Movie
-            //     {
-            //                Title = "Nyckeln till Frihet",
-            //                OriginalTitle = "Shawshank Redemption",
-            //                ReleaseYear = 1994,
-            //                Rating = 9.2,
-            //                Synopsis =
-            //                    "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-            //                Genre = "Crime, Drama",
-            //                Actors = "Tim Robbins, Morgan Freeman, Bob Gunton"
-            //            },
-            //            new Movie
-            //            {
-            //                Title = "Gudfadern",
-            //                OriginalTitle = "The Godfather",
-            //                ReleaseYear = 1972,
-            //                Rating = 9.2,
-            //                Synopsis =
-            //                    "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-            //                Genre = "Crime, Drama",
-            //                Actors = "Marlon Brando, Al Pacino, James Caan"
-            //            },
-            //            new Movie
-            //            {
-            //                Title = "Gudfadern del II",
-            //                OriginalTitle = "The Godfather: Part II",
-            //                ReleaseYear = 1974,
-            //                Rating = 9.0,
-            //                Synopsis =
-            //                    "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
-            //                Genre = "Crime, Drama",
-            //                Actors = "Al Pacino, Robert De Niro, Robert Duvall"
-            //            },
-            //            new Movie
-            //            {
-            //                Title = "The Dark Knight",
-            //                OriginalTitle = "The Dark Knight",
-            //                ReleaseYear = 2008,
-            //                Rating = 9.0,
-            //                Synopsis =
-            //                    "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-            //                Genre = "Action, Crime, Thriller",
-            //                Actors = "Christian Bale, Heath Ledger, Aaron Eckhart"
-            //            },
-            //            new Movie
-            //            {
-            //                Title = "12 edsvurna män",
-            //                OriginalTitle = "12 Angry Men",
-            //                ReleaseYear = 1957,
-            //                Rating = 8.9,
-            //                Synopsis =
-            //                    "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
-            //                Genre = "Crime, Drama",
-            //                Actors = "Henry Fonda, Lee J. Cobb, Martin Balsam"
-            //            }
-            //};
-            //seededMovies.ForEach(m => context.Movies.Add(m));
+            var seededMovies = new List<Movie>
+            {
+                 new Movie
+                 {
+                            Title = "Nyckeln till Frihet",
+                            OriginalTitle = "Shawshank Redemption",
+                            ReleaseYear = 1994,
+                            Rating = 9.2,
+                            Synopsis =
+                                "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                            Genre = "Crime, Drama",
+                            Actors = "Tim Robbins, Morgan Freeman, Bob Gunton"
+                        },
+                        new Movie
+                        {
+                            Title = "Gudfadern",
+                            OriginalTitle = "The Godfather",
+                            ReleaseYear = 1972,
+                            Rating = 9.2,
+                            Synopsis =
+                                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+                            Genre = "Crime, Drama",
+                            Actors = "Marlon Brando, Al Pacino, James Caan"
+                        },
+                        new Movie
+                        {
+                            Title = "Gudfadern del II",
+                            OriginalTitle = "The Godfather: Part II",
+                            ReleaseYear = 1974,
+                            Rating = 9.0,
+                            Synopsis =
+                                "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+                            Genre = "Crime, Drama",
+                            Actors = "Al Pacino, Robert De Niro, Robert Duvall"
+                        },
+                        new Movie
+                        {
+                            Title = "The Dark Knight",
+                            OriginalTitle = "The Dark Knight",
+                            ReleaseYear = 2008,
+                            Rating = 9.0,
+                            Synopsis =
+                                "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+                            Genre = "Action, Crime, Thriller",
+                            Actors = "Christian Bale, Heath Ledger, Aaron Eckhart"
+                        },
+                        new Movie
+                        {
+                            Title = "12 edsvurna män",
+                            OriginalTitle = "12 Angry Men",
+                            ReleaseYear = 1957,
+                            Rating = 8.9,
+                            Synopsis =
+                                "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+                            Genre = "Crime, Drama",
+                            Actors = "Henry Fonda, Lee J. Cobb, Martin Balsam"
+                        }
+            };
+            seededMovies.ForEach(m => context.Movies.Add(m));
 
             context.SaveChanges();
         }
--- MVC_Lab3/Models/Movie.cs
+++ MVC_Lab3/Models/Movie.cs
@@ -8,6 +8,54 @@ namespace MVC_Lab3.Models
 {
     public class Movie
     {
-        //Add your properties here.
+        string title;
+        string originalTitle;
+        int releaseYear;
+        double rating;
+        string synopsis;
+        string genre;
+        string actors;
+        // List<string> genre;
+        // List<string> actors;
+        //int MovieID;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string OriginalTitle
+        {
+            get { return originalTitle; }
+            set { originalTitle = value; }
+        }
+        public int ReleaseYear
+        {
+            get { return releaseYear; }
+            set { releaseYear = value; }
+        }
+        public double Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+        public string Synopsis
+        {
+            get { return synopsis; }
+            set { synopsis = value; }
+        }
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+        public string Actors
+        {
+            get { return actors; }
+            set { actors = value; }
+        }
+        public int MovieID { get; set; }
+
+
     }
 }
